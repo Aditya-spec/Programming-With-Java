261be8c92131a076fa1ebce6ca0211b88f17eefe
Aditya-spec <57364486+Aditya-spec@users.noreply.github.com>
2021-02-16 01:49:09 +0530

    Update Third.java

--- JAVA 1/Third.java
+++ JAVA 1/Third.java
@@ -4,23 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 // Write a program to find the number of occurrences of a word in a string without using loop?
 public class Third {
-    public static void main(String[] args) {
-        String s="big black bug bit a big black dog on his big black nose";
-        char[] characters = s.toCharArray();
-        HashMap<Character, Integer> m=new HashMap<Character, Integer>();
-        for (int i=0;i< characters.length;i++){
 
-            if(m.containsKey(characters[i])==true)
-            {
-                m.put(characters[i],m.get(characters[i]) + 1);
-            }
-            else
-                m.put(characters[i],1 );
-        }
-        for (Map.Entry<Character,Integer> e: m.entrySet())
+        public static void main(String[] args)
         {
-            if(e.getValue() > 1)
-                System.out.println(e.getKey()+ " " +e.getValue());
+            String str = "Java why java how java now java";
+            int count = str.length() - str.replace("a", "").length();
+            System.out.println("Number of occurances of 'a' in"+str+"  = "+count);
         }
     }
-}
