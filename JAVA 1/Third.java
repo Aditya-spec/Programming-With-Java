7d28c6260735ba839be0ad917dddbf4cbed76fa5
Aditya-spec <57364486+Aditya-spec@users.noreply.github.com>
2021-02-16 01:49:38 +0530

    Update Third.java

--- JAVA 1/Third.java
+++ JAVA 1/Third.java
@@ -1,8 +1,8 @@
-// Write a program to find the number of occurrences of a word
+// Write a program to find the number of occurrences of a character
 // in a string without using loop?
 import java.util.HashMap;
 import java.util.Map;
-// Write a program to find the number of occurrences of a word in a string without using loop?
+
 public class Third {
 
         public static void main(String[] args)
