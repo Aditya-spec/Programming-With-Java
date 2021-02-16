c88ed9b8e5230d0eef4ef3c1f1b81b13e324f558
Aditya-spec <57364486+Aditya-spec@users.noreply.github.com>
2021-02-16 11:54:55 +0530

    Update EntryClass.java

--- JAVA 1/eleventh/EntryClass.java
+++ JAVA 1/eleventh/EntryClass.java
@@ -4,10 +4,11 @@ public class EntryClass {
     public static void main(String[] args) {
         Bank b;
         b=new SBI();
-        System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");
+        b.getDetails("SBI");
         b=new BOI();
-        System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");
+       b.getDetails("BOI");
         b=new ICICI();
-        System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");
+       b.getDetails("ICICI");
     }
 }
+
