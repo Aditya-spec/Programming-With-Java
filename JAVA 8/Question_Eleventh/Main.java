c6a38016beb1612becfa59c2c42dbd49cc888e93
Aditya-spec <57364486+Aditya-spec@users.noreply.github.com>
2021-02-19 13:18:02 +0530

    Update Main.java

--- JAVA 8/Question_Eleventh/Main.java
+++ JAVA 8/Question_Eleventh/Main.java
@@ -1,14 +1,17 @@
 //Find average of the number inside integer list after doubling it.
 package Question_Eleventh;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers= List.of(2,5,3,7,8,4,6);
-       int number = numbers.stream()
+        List<Integer> numbers= Arrays.asList(2,4,5,6,7,3,5,7);
+        int length=numbers.size();
+        System.out.println(length);
+       float number = numbers.stream()
                 .map(x->2*x)
-                .reduce(0 , Integer::sum);
+                .reduce(0 , Integer::sum)/length;
        System.out.println(number);
 
     }
