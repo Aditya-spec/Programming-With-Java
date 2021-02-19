263818bce3582398b86f9ed387e2694194bd3b1a
Aditya-spec <57364486+Aditya-spec@users.noreply.github.com>
2021-02-19 19:12:20 +0530

    Update Main.java

--- JAVA 8/Question_Eleventh/Main.java
+++ JAVA 8/Question_Eleventh/Main.java
@@ -9,9 +9,10 @@ public class Main {
         List<Integer> numbers= Arrays.asList(2,4,5,6,7,3,5,7);
         int length=numbers.size();
         System.out.println(length);
-       float number = numbers.stream()
-                .map(x->2*x)
-                .reduce(0 , Integer::sum)/length;
+       double number = numbers.stream()
+                      .mapToDouble(x->x*2)
+                       .average()
+               .orElse(0.0);
        System.out.println(number);
 
     }
